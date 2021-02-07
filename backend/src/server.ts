7a31c9305aa434fc40af5d011f96516256a9c3d5
ozbe <1372945+ozbe@users.noreply.github.com>
2021-02-07 19:49:21 +1100

    remove backend tests, readme, and docker; switch from lambda to cloud funciton

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,15 @@
-// FIXME - deprecate ApolloServer -there are two ApolloServers to support dev run and lambda run
-import { ApolloServer } from 'apollo-server'
-import { ApolloServer as ApolloServerLambda } from 'apollo-server-lambda'
+import { ApolloServer } from 'apollo-server-cloud-functions'
 import { context } from './context';
 import { schema } from './schema'
 
-export const server = new ApolloServer({ 
+const server = new ApolloServer({ 
   schema,
   context,
-  playground: {
-    endpoint: "/dev/graphql"
-  }
+  playground: true,
+  introspection: true,
 })
 
-const lambdaServer = new ApolloServerLambda({ 
-  schema,
-  context,
-  playground: {
-    endpoint: "/dev/graphql"
-  }
-})
-
-// https://www.apollographql.com/docs/apollo-server/deployment/lambda/
-export const handler = lambdaServer.createHandler({
+export const handler = server.createHandler({
   cors: {
     origin: true,
     credentials: true,
